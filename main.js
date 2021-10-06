63b7a4bfad43dea8ba6ed212b3e6dd6bdc15a80b
Albab05 <83689371+Albab05@users.noreply.github.com>
2021-10-06 21:05:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
-const port = process.env.PORT || 8000;
+const port = process.env.PORT | 8000;
 const app = express();
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 const connection = mysql.createConnection({
